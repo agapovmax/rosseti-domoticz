5bef474b3907cc3384bda87797e86750a06970de
Agapov Max <ddtpv@mail.ru>
2021-09-27 09:54:00 +0300

    Add address from idx

--- rosseti-domoticz.py
+++ rosseti-domoticz.py
@@ -1,3 +1,4 @@
+from re import split
 import requests
 import datetime
 import config
@@ -13,6 +14,23 @@ currdate = datetime.datetime.today()
 todate = (datetime.datetime.today()).strftime("%d.%m.%Y")
 fromdate = (currdate - datetime.timedelta(days=int(days))).strftime("%d.%m.%Y")
 
+# Достаем адрес из названия виртуального датчика (idx) в domoticz
+d_url = 'http://' + conf['domoticz_server'] + ':' + conf['domoticz_port'] + '/json.htm?'
+d_headers = {
+    'User-Agent' : 'curl/7.61.0',
+    'Charset' : 'UTF-8'
+}
+d_params = {
+    'type' : 'devices',
+    'rid' : conf['domoticz_idx']
+}
+d_check = requests.get(d_url, headers=d_headers, params=d_params, auth=HTTPBasicAuth(conf['username'], conf['password'])).json()
+
+for item in d_check['result']:
+    d_address = item['Name']
+    print('Название датчика (адрес): ' + d_address)
+    d_address = (d_address.split(sep=":"))[1]
+
 # Проверяем новые события на МРСК с настройками из conf.cfg
 url = 'https://lk.mrsksevzap.ru/Ajax/Interruptions?region=&district=&settlement=&manualSettlement=&street=&isManualStreet=true&manualStreet=&house=&page=1&fieldName=OutageDate&orderDirection=1'
 headers = {
@@ -20,7 +38,7 @@ headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     }
 params = {
-    'fullAddress': conf['fulladdress'] + ', ' + conf['street'] + ', ' + conf['house'],
+    'fullAddress': conf['fulladdress'] + ', ' + d_address,
     'region':  conf['region'],
     'isSettlement': 'true',
     'from': fromdate,
@@ -53,8 +71,7 @@ d_check = requests.get(d_url, headers=d_headers, params=d_params, auth=HTTPBasic
 
 for item in d_check['result']:
     d_result = item['Data']
-    print('Текущая запись в домотикз')
-    print(d_result)
+    print('Текущая запись в Domoticz: ' + d_result)
 
 # Обновляем idx в domoticz
 du_params = {
@@ -68,7 +85,6 @@ du_params = {
 if result == 0:
     print('Нет новых событий!')
 else:
-    print('Запись найдена!')
     if d_result != result:
         print('Обновляем запись...')
         # Пишем в файл данные
